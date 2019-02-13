86bb26fb01b60486729fccc710faff0a9149f6ff
La Fountain <github email address>
2019-02-13 18:14:53 -0500

    update auth controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,10 +3,13 @@ class AuthController < ApplicationController
  skip_before_action :authorized, only: [:authenticate]
  
   def authenticate
+   
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       token = encode_token({ user_id: user.id})
       render json: {user: user, jwt: token }
+    else
+    render json: { error: "Login Error"}, status: :unauthorized
     end
   end
 end 
